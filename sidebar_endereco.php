69eb5aa88eabf8e093282d24d634e03ae172556f
Francisco7777777 <83313636+Francisco7777777@users.noreply.github.com>
2021-04-28 21:39:51 -0300

    Update sidebar_endereco.php

--- sidebar_endereco.php
+++ sidebar_endereco.php
@@ -2,7 +2,7 @@
 	
 <section>
 	<header class="major">
-		<h2> <?php echo $ENDEREÇO ?> </h2>
+		<h2> <?php echo $ENDERECO ?> </h2>
 	</header>
 	<p>Sed varius enim lorem ullamcorper dolore aliquam aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin sed aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
 	<ul class="contact">
